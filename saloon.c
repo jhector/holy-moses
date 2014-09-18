6155757a77f6affaeec2b6e345b831ac2924499d
Julian Hector <jhector@mozilla.com>
2014-09-18 19:33:46 +0200

    added default log stream

--- saloon.c
+++ saloon.c
@@ -4,17 +4,19 @@
 #include <stdint.h>
 #include <unistd.h>
 
+#define LOG_STREAM stdout
+
 void log_msg(char *fmt, ...)
 {
     va_list args;
     va_start(args, fmt);
 
-    vprintf(fmt, args);
+    vfprintf(LOG_STREAM, fmt, args);
 }
 
 void die(char *error)
 {
-    log_msg("Fatal error: %s\n", error);
+    log_msg("FATAL: %s\n", error);
     exit(-1);
 }
 
