6174b487b29721e92b2407cc306eefebf8b4d878
Julian Hector <jhector@mozilla.com>
2014-09-18 21:21:48 +0200

    added functions for request invite

--- saloon.c
+++ saloon.c
@@ -8,9 +8,18 @@
 #include <unistd.h>
 #include <string.h>
 
+#define REF_SIZE 64
 #define LOG_STREAM stdout
 
+struct request {
+    const char *name;
+    uint64_t age;
+};
+
 static int32_t log_fd = -1;
+
+struct request last_requests[20];
+uint32_t idx = 0;
 
 void log_file(char *msg)
 {
@@ -54,13 +63,13 @@ void menu()
 
 int32_t check_referal(const char *code)
 {
-    char ref[32] = {0};
+    char ref[REF_SIZE] = {0};
 
     int32_t fd = open("referals", O_RDONLY);
     if (fd < 0)
         return 0;
 
-    if (read(fd, ref, sizeof(ref)-1) <= 0)
+    if (read(fd, ref, 12) <= 0)
         goto fail_close;
 
     if (!strcmp(code, ref)) {
@@ -77,9 +86,9 @@ fail_close:
 
 void enter_referal()
 {
-    char code[32] = {0};
+    char code[REF_SIZE] = {0};
 
-    _write("Code: ");
+    _write("Code (12 bytes): ");
 
     if (read(0, code, sizeof(code)-1) < 0)
         die("reading referal code");
@@ -88,6 +97,48 @@ void enter_referal()
         log_msg("Your code: %s, is valid\nPlease contact your referal\n", code);
     } else {
         log_msg("Invalid code: %s\n", code);
+    }
+}
+
+void store_last(const char *name, uint64_t age)
+{
+    last_requests[idx % 20].name = name;
+    last_requests[idx % 20].age = age;
+    idx++;
+}
+
+int32_t check_request(const char *name, uint64_t age)
+{
+    char msg[50] = {0};
+
+    snprintf(msg, 22 + strlen(name), "Blocked request from %s", name);
+
+    log_file(msg);
+
+    store_last(name, age);
+    return 0;
+}
+
+void request_invite()
+{
+    char buf[28];
+    uint64_t age;
+    char name[28];
+
+    _write("Age: ");
+    if (read(0, buf, sizeof(name)-1) < 0)
+        return;
+
+    age = strtoull(buf, NULL, 10);
+
+    _write("Name: ");
+    if (read(0, name, sizeof(name)-1) < 0)
+        return;
+
+    if (check_request(name, age)) {
+        log_msg("Thank you %sYour invitation will be mailed to you.\n", name);
+    } else {
+        log_msg("We are sorry %sThere is no more room.\n", name);
     }
 }
 
@@ -113,6 +164,7 @@ int32_t main()
             enter_referal();
             break;
         case 2:
+            request_invite();
             break;
         case 3:
             goto leave;
